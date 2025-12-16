a58972896086bf31be7e348ae432355743c87d11
Pimenta <spimen01@blueshieldca.com>
2025-12-15 17:58:20 -0800

    add config, db and wh

--- main.tf
+++ main.tf
@@ -1,96 +1,41 @@
 terraform {
   required_providers {
     snowflake = {
-      source  = "Snowflake-Labs/snowflake"
-      version = "~> 0.68"
+      source = "snowflakedb/snowflake"
     }
   }
 }
 
+locals {
+  organization_name = "oggscuh"
+  account_name      = "itb80607"
+  private_key_path  = "~/.ssh/snowflake_tf_snow_key.p8"
+}
+
 provider "snowflake" {
-  role = "SYSADMIN"
+    organization_name = local.organization_name
+    account_name      = local.account_name
+    user              = "TERRAFORM_SVC"
+    role              = "SYSADMIN"
+    authenticator     = "SNOWFLAKE_JWT"
+    private_key       = file(local.private_key_path)
 }
 
-resource "snowflake_database" "db" {
-  name = "TF_DEMO"
+resource "snowflake_database" "tf_db" {
+  name         = "TF_DEMO_DB"
+  is_transient = false
 }
 
-resource "snowflake_warehouse" "warehouse" {
-  name           = "TF_DEMO"
-  warehouse_size = "small"
-  auto_suspend   = 60
-}
-provider "snowflake" {
-  alias = "security_admin"
-  role  = "SECURITYADMIN"
-}
-
-resource "snowflake_role" "role" {
-  provider = snowflake.security_admin
-  name     = "TF_DEMO_SVC_ROLE"
+resource "snowflake_warehouse" "tf_warehouse" {
+  name                      = "TF_DEMO_WH"
+  warehouse_type            = "STANDARD"
+  warehouse_size            = "XSMALL"
+  max_cluster_count         = 1
+  min_cluster_count         = 1
+  auto_suspend              = 60
+  auto_resume               = true
+  enable_query_acceleration = false
+  initially_suspended       = true
 }
 
-resource "snowflake_grant_privileges_to_role" "database_grant" {
-  provider   = snowflake.security_admin
-  privileges = ["USAGE"]
-  role_name  = snowflake_role.role.name
-  on_account_object {
-    object_type = "DATABASE"
-    object_name = snowflake_database.db.name
-  }
-}
 
-resource "snowflake_schema" "schema" {
-  database   = snowflake_database.db.name
-  name       = "TF_DEMO"
-  is_managed = false
-}
-
-resource "snowflake_grant_privileges_to_role" "schema_grant" {
-  provider   = snowflake.security_admin
-  privileges = ["USAGE"]
-  role_name  = snowflake_role.role.name
-  on_schema {
-    schema_name = "\"${snowflake_database.db.name}\".\"${snowflake_schema.schema.name}\""
-  }
-}
-
-resource "snowflake_grant_privileges_to_role" "warehouse_grant" {
-  provider   = snowflake.security_admin
-  privileges = ["USAGE"]
-  role_name  = snowflake_role.role.name
-  on_account_object {
-    object_type = "WAREHOUSE"
-    object_name = snowflake_warehouse.warehouse.name
-  }
-}
-
-resource "tls_private_key" "svc_key" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
-}
-
-resource "snowflake_user" "user" {
-    provider          = snowflake.security_admin
-    name              = "tf_demo_user"
-    default_warehouse = snowflake_warehouse.warehouse.name
-    default_role      = snowflake_role.role.name
-    default_namespace = "${snowflake_database.db.name}.${snowflake_schema.schema.name}"
-    rsa_public_key    = substr(tls_private_key.svc_key.public_key_pem, 27, 398)
-}
-
-resource "snowflake_grant_privileges_to_role" "user_grant" {
-  provider   = snowflake.security_admin
-  privileges = ["MONITOR"]
-  role_name  = snowflake_role.role.name
-  on_account_object {
-    object_type = "USER"
-    object_name = snowflake_user.user.name
-  }
-}
-
-resource "snowflake_role_grants" "grants" {
-  provider  = snowflake.security_admin
-  role_name = snowflake_role.role.name
-  users     = [snowflake_user.user.name]
-}
